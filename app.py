1e02feb45bf5d22087e64f141847e3041841e1d9
Devvrat Hans <devvrat.coding@gmail.com>
2025-03-11 15:35:06 +0530

    Add code files

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pickle
+import pandas as pd
+
+movies_list = pickle.load(open('movies_list.pkl', 'rb'))    
+movies_list = movies_list['title'].values
+
+st.title('Movie Recommendation System')
+
+option = st.selectbox('Select a movie:', movies_list)
+
+st.write('You selected:', option)
+
